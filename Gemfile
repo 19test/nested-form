89f2b722426eb1bf5f1a92a7ab412e7c84c3c695
seyyah <nurettin.senyer@bil.omu.edu.tr>
2012-04-04 08:28:39 +0300

    ilk

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.2'
+gem 'jquery-rails'
+gem 'execjs'
+gem 'therubyracer'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development,:test do
+  gem 'sqlite3'
+end
+group :production do
+  gem 'pg', '0.12.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveysays::Application.config.secret_token = '96b433b0cd2521dc883df0d1c3ac47da0a1b8b827b70c9c1d8b7f431854a079b6989fd39b23f241eb2f7ea41daead1cd9e52c6b59446ab9294c0be98e0665fa3'
